815c2887683b121e20b14d83c50b4e7f28d0a853
TempleLo <templelo5408@gmail.com>
2022-05-21 16:30:09 +0800

    Episode 01.

---
+++ _01_getting_started/LaunchAndExecuteMoonSharp/LaunchAndExecuteMoonSharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using MoonSharp.Interpreter;
+
+namespace LaunchAndExecuteMoonSharp {
+    internal class Program {
+        static void Main(string[] args) {
+            Console.WriteLine(rtnHelloWorld());
+        }
+
+        static String rtnHelloWorld() {
+            String script = @"
+                return 'Hello world!'    
+            ";
+            DynValue res = Script.RunString(script);
+            return res.String;
+        }
+    }
+}
